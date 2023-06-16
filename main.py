21ab5388d1b2d4f212380c97784de6e3555c7bed
Bibi <beaves83@gmail.com>
2023-06-16 14:04:47 +0200

    feat: Add CORS middleware for local development

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 db_connection = psycopg2.connect(
     host="localhost",
