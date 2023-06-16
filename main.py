24afe6b311f5b6e91a36cbe63e643352b4e69181
Bibi <beaves83@gmail.com>
2023-06-16 12:05:01 +0200

    feat: added FastAPI routes for CRUD operations

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from fastapi import FastAPI 
 
-add = FastAPI()
+app = FastAPI()
+
+@app.get("/items")
+async def get_items():
+    items = []
+    return items
+
+@app.post("/items")
+async def create_item(item: dict):
+    return {"message": "Item created successfully"}
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    return {"message": f"Item {item_id} deleted successfully"}
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: dict):
+    return {"message": f"Item {item_id} updated successfully"}
