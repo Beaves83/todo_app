6475a2ba7d5c7a30bfe306b4dc6b411ead70e9c7
Bibi <beaves83@gmail.com>
2023-06-19 02:21:52 +0200

    refactor: create new component AddItemForm

---
+++ todoapp-frontend/src/components/AddItemForm.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import { ref, defineEmits } from "vue";
+
+const newItem = ref({ title: "", description: "", completed: false });
+
+const emit = defineEmits(["add"]);
+
+const addItem = () => {
+  emit("add", newItem.value);
+  newItem.value = { title: "", description: "", completed: false };
+};
+</script>
+<template>
+  <div class="add-item">
+    <div class="add-item__header">
+      <h3 class="add-item__title">Add new item</h3>
+    </div>
+    <form @submit.prevent="addItem" class="add-item__form">
+      <div class="add-item__form-group">
+        <label for="title" class="add-item__label">Title</label>
+        <input
+          type="text"
+          id="title"
+          v-model="newItem.title"
+          class="add-item__input"
+        />
+      </div>
+      <div class="add-item__form-group">
+        <label for="description" class="add-item__label">Description</label>
+        <input
+          type="text"
+          id="description"
+          v-model="newItem.description"
+          class="add-item__input"
+        />
+      </div>
+      <button type="submit" class="add-item__button">Add item</button>
+    </form>
+  </div>
+</template>
+<style scoped>
+.add-item {
+  margin-top: 2rem;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.add-item__header {
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+}
+
+.add-item__title {
+  font-weight: bold;
+  margin-right: 1rem;
+}
+
+.add-item__form {
+  display: flex;
+  flex-direction: column;
+}
+
+.add-item__form-group {
+  margin-bottom: 1rem;
+}
+
+.add-item__label {
+  font-weight: bold;
+  margin-right: 20px;
+}
+
+.add-item__input {
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 100%;
+}
+
+.add-item__button {
+  padding: 0.5rem;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+.add-item__button:hover {
+  background-color: #45a049;
+}
+
+.add-item__button:active {
+  background-color: #3e8e41;
+}
+</style>
--- todoapp-frontend/src/components/ItemList.vue
+++ todoapp-frontend/src/components/ItemList.vue
@@ -3,6 +3,7 @@ import { ref, computed } from "vue";
 import axios from "axios";
 import { Item } from "./types";
 import SearchInput from "./SearchInput.vue";
+import AddItemForm from "./AddItemForm.vue";
 
 // Constants
 const searchQuery = ref("");
@@ -31,9 +32,9 @@ const fetchItems = () => {
     });
 };
 
-const addItem = () => {
+const addItem = (item: Item) => {
   axios
-    .post(url, newItem.value)
+    .post(url, item)
     .then(() => {
       fetchItems();
       newItem.value = { title: "", description: "", completed: false };
@@ -129,32 +130,7 @@ const updateSearchQuery = (newValue: string) => {
         </tr>
       </tbody>
     </table>
-    <div class="add-item">
-      <div class="add-item__header">
-        <h3 class="add-item__title">Add new item</h3>
-      </div>
-      <form @submit.prevent="addItem" class="add-item__form">
-        <div class="add-item__form-group">
-          <label for="title" class="add-item__label">Title</label>
-          <input
-            type="text"
-            id="title"
-            v-model="newItem.title"
-            class="add-item__input"
-          />
-        </div>
-        <div class="add-item__form-group">
-          <label for="description" class="add-item__label">Description</label>
-          <input
-            type="text"
-            id="description"
-            v-model="newItem.description"
-            class="add-item__input"
-          />
-        </div>
-        <button type="submit" class="add-item__button">Add item</button>
-      </form>
-    </div>
+    <AddItemForm @add="addItem" />
   </div>
 </template>
 <style scoped>
@@ -167,62 +143,6 @@ const updateSearchQuery = (newValue: string) => {
   font-size: 2rem;
   font-weight: bold;
   margin: 2rem;
-}
-
-.add-item {
-  margin-top: 2rem;
-  padding: 1rem;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-
-.add-item__header {
-  display: flex;
-  align-items: center;
-  margin-bottom: 1rem;
-}
-
-.add-item__title {
-  font-weight: bold;
-  margin-right: 1rem;
-}
-
-.add-item__form {
-  display: flex;
-  flex-direction: column;
-}
-
-.add-item__form-group {
-  margin-bottom: 1rem;
-}
-
-.add-item__label {
-  font-weight: bold;
-  margin-right: 20px;
-}
-
-.add-item__input {
-  padding: 0.5rem;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  width: 100%;
-}
-
-.add-item__button {
-  padding: 0.5rem;
-  background-color: #4caf50;
-  color: white;
-  border: none;
-  border-radius: 8px;
-  cursor: pointer;
-}
-
-.add-item__button:hover {
-  background-color: #45a049;
-}
-
-.add-item__button:active {
-  background-color: #3e8e41;
 }
 
 .table__item {
